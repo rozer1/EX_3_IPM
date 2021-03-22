af17a65724668624a91729a96bc2a6193313e580
Tobiasz <209458@edu.p.lodz.pl>
2021-03-22 13:08:52 +0100

    Button i platforma

--- myscript.js
+++ myscript.js
@@ -1,48 +1,19 @@
-      function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-      }
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
 
-      function spawnBox(stage, layer) {
-        var rectX = stage.width() / 2 - 50;
-        var rectY = stage.height() / 2 - 25;
-        var color = getRandomColor();
-        var box = new Konva.Rect({
-          x: rectX,
-          y: rectY,
-          width: 50,
-          height: 50,
-          fill: color,
-          stroke: 'black',
-          strokeWidth: 4,
-          draggable: true,
-        });
-        layer.add(box);
-        box.on('mouseover', function () {
-          document.body.style.cursor = 'pointer';
-        });
-        box.on('mouseout', function () {
-          document.body.style.cursor = 'default';
-        });
-        return box;
-      }
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
 
-      var width = window.innerWidth;
-      var height = window.innerHeight;
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
 
-      var stage = new Konva.Stage({
-        container: 'container',
-        width: width,
-        height: height,
-      });
-		
-		var layer = new Konva.Layer();
-		spawnBox(stage, layer);
-		spawnBox(stage, layer);
-		spawnBox(stage, layer);
-       
-      stage.add(layer);
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
